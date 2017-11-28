3b676588e20590c99e95af2067e519c0b65296c5
Angat Lamichhane <angatlamichhane@gmail.com>
2017-11-28 11:10:18 -0600

    filesAdded

---
+++ LamichhaneAngat_AddressBooks/AddressBook.aspx
@@ -0,0 +1,135 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddressBook.aspx.cs" Inherits="LamichhaneAngat_AddressBooks.AddressBook" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            height: 23px;
+        }
+        .auto-style3 {
+            height: 30px;
+        }
+        .auto-style4 {
+            width: 105px;
+        }
+        .auto-style5 {
+            height: 30px;
+            width: 105px;
+        }
+        .auto-style6 {
+            height: 23px;
+            width: 105px;
+        }
+        .auto-style7 {
+            margin-left: 0px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style4">First Name:
+                    </td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server" Width="232px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">Last name:
+                    </td>
+                <td>
+                    <asp:TextBox ID="TextBox2" runat="server" Width="231px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">Email:
+                    </td>
+                <td>
+                    <asp:TextBox ID="TextBox3" runat="server" Width="230px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style5">Phone:&nbsp;
+                    </td>
+                <td class="auto-style3">
+                    <asp:TextBox ID="TextBox4" runat="server" Width="228px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                   
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                   
+                </td>
+                <td class="auto-style4"></td>
+            </tr>
+            <tr>
+                <td class="auto-style6">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</td>
+                <td class="auto-style2">
+                    <asp:Button ID="NextButton" runat="server" OnClick="NextButton_Click" Text="Next" Width="54px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <asp:Button ID="PreviousButton" runat="server" OnClick="PreviousButton_Click" Text="Previous" />
+&nbsp;&nbsp;&nbsp;&nbsp;
+                  
+                    <asp:Button ID="FirstButton" runat="server" OnClick="FirstButton_Click" Text="First" Width="48px" />
+                  
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style6">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</td>
+                <td class="auto-style2">
+                    <asp:Button ID="ClearButton" runat="server" OnClick="ClearButton_Click" Text="Clear" Width="54px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <asp:Button ID="AddButton" runat="server" OnClick="AddButton_Click" Text="Add" Width="75px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <asp:Button ID="FindButton" runat="server" CssClass="auto-style7" OnClick="FindButton_Click" Text="Find" Width="47px" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style6">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;</td>
+                <td class="auto-style2">
+                    <asp:Button ID="DeleteButton" runat="server" OnClick="DeleteButton_Click" Text="Delete" Height="26px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <asp:Button ID="UpdateButton" runat="server" OnClick="UpdateButton_Click" Text="Update" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style6">&nbsp;</td>
+                <td class="auto-style2">
+                    <asp:TextBox ID="TextBox5" runat="server" OnTextChanged="TextBox5_TextChanged" Visible="False" Width="205px"></asp:TextBox>
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" DeleteCommand="DELETE FROM [Addresses] WHERE [AddressID] = @AddressID" InsertCommand="INSERT INTO [Addresses] ([FirstName], [LastName], [Email], [PhoneNumber]) VALUES (@FirstName, @LastName, @Email, @PhoneNumber)" SelectCommand="SELECT * FROM [Addresses]" UpdateCommand="UPDATE [Addresses] SET [FirstName] = @FirstName, [LastName] = @LastName, [Email] = @Email, [PhoneNumber] = @PhoneNumber WHERE [AddressID] = @AddressID">
+            <DeleteParameters>
+                <asp:Parameter Name="AddressID" Type="Int32" />
+            </DeleteParameters>
+            <InsertParameters>
+                <asp:Parameter Name="FirstName" Type="String" />
+                <asp:Parameter Name="LastName" Type="String" />
+                <asp:Parameter Name="Email" Type="String" />
+                <asp:Parameter Name="PhoneNumber" Type="String" />
+            </InsertParameters>
+            <UpdateParameters>
+                <asp:Parameter Name="FirstName" Type="String" />
+                <asp:Parameter Name="LastName" Type="String" />
+                <asp:Parameter Name="Email" Type="String" />
+                <asp:Parameter Name="PhoneNumber" Type="String" />
+                <asp:Parameter Name="AddressID" Type="Int32" />
+            </UpdateParameters>
+        </asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ LamichhaneAngat_AddressBooks/AddressBook.aspx.cs
@@ -0,0 +1,201 @@
+﻿using LamichhaneAngat_AddressBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LamichhaneAngat_AddressBooks
+{
+    public partial class AddressBook : System.Web.UI.Page
+    {
+        AddressBookEntities dbcontext = new AddressBookEntities();
+
+        string connStr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        SqlCommand com;
+        string str;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connStr);
+            con.Open();
+            str = "select * from Addresses";
+            com = new SqlCommand(str, con);
+            SqlDataReader reader = com.ExecuteReader();
+            if (reader.Read())
+            {
+                TextBox1.Text = (reader["FirstName"].ToString());
+                TextBox2.Text = (reader["LastName"].ToString());
+                TextBox3.Text = (reader["Email"].ToString());
+                TextBox4.Text = (reader["PhoneNumber"].ToString());
+                reader.Close();
+                con.Close();
+            }
+            
+
+
+        }
+        private static int getID = 1;
+        protected void NextButton_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connStr);
+            con.Open();
+            List<Address> addresses = dbcontext.Addresses.ToList();
+            int rowCount = addresses.Count;
+            com = new SqlCommand(str, con);
+            SqlDataReader reader = com.ExecuteReader();
+            if (getID >= 1 && getID < rowCount)
+            {
+                getID++;
+                com = new SqlCommand("SELECT * FROM (SELECT ROW_NUMBER() OVER (ORDER BY AddressID ASC) AS rownum,FirstName,LastName,Email,PhoneNumber FROM ADDRESSES) AS foo WHERE rownum =" + getID + "", con);
+                reader = com.ExecuteReader();
+                if (reader.Read())
+                {
+                    TextBox1.Text = (reader["FirstName"].ToString());
+                    TextBox2.Text = (reader["LastName"].ToString());
+                    TextBox3.Text = (reader["Email"].ToString());
+                    TextBox4.Text = (reader["PhoneNumber"].ToString());
+                    reader.Close();
+                    con.Close();
+                }
+                con.Close();
+                TextBox5.Visible = true;
+                TextBox5.Text = "This is entry number " + getID;
+
+            }
+            else {
+                getID = 1;
+                TextBox5.Visible = true;
+                TextBox5.Text = "This is entry number " + getID;
+            }
+            
+        }
+
+        protected void PreviousButton_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connStr);
+            con.Open();
+            List<Address> addresses = dbcontext.Addresses.ToList();
+            int rowCount = addresses.Count;
+            com = new SqlCommand(str, con);
+            SqlDataReader reader = com.ExecuteReader();
+
+            if (getID >= 1 && getID <= rowCount+1)
+            {
+                getID--;
+                com = new SqlCommand("SELECT * FROM (SELECT ROW_NUMBER() OVER (ORDER BY AddressID ASC) AS rownum,FirstName,LastName,Email,PhoneNumber FROM ADDRESSES) AS foo WHERE rownum =" + getID + "", con);
+                reader = com.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    TextBox1.Text = (reader["FirstName"].ToString());
+                    TextBox2.Text = (reader["LastName"].ToString());
+                    TextBox3.Text = (reader["Email"].ToString());
+                    TextBox4.Text = (reader["PhoneNumber"].ToString());
+                    reader.Close();
+                    con.Close();
+                    TextBox5.Text = "This is entry number " + getID;
+                }
+                else
+                {
+                    getID = rowCount + 1;
+                }
+
+
+            }
+        }
+
+       
+    
+        protected void ClearButton_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = String.Empty;
+            TextBox2.Text = String.Empty;
+            TextBox3.Text = String.Empty;
+            TextBox4.Text = String.Empty;
+            TextBox5.Visible = false;
+        }
+
+        protected void AddButton_Click(object sender, EventArgs e)
+        {
+            /*
+            {
+                Address message = new Address();
+                message.FirstName = TextBox1.Text;
+                message.LastName = TextBox2.Text;
+                message.Email = TextBox3.Text;
+                message.PhoneNumber = TextBox4.Text;
+
+                dbcontext.Addresses.Add(message);
+                dbcontext.SaveChanges();
+            }
+           ClearButton_Click(sender, e);
+          */
+            SqlConnection con = new SqlConnection(connStr);
+            try
+            {
+                string query = "insert into Addresses(FirstName,LastName,Email,PhoneNumber) values('" + TextBox1.Text + "', '" + TextBox2.Text + "', '" + TextBox3.Text + "', '" + TextBox4.Text + "'); ";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                con.Open();
+                da.SelectCommand.ExecuteNonQuery();
+                con.Close();
+                TextBox5.Text = "Data saved successfully.";
+            }
+            catch
+            {
+                con.Close();
+                TextBox5.Text = "Error while saving data.";
+            }
+
+        }
+
+        protected void FindButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DeleteButton_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connStr);
+
+            string sqlStatement = "DELETE FROM Addresses WHERE AddressID = @AddressID";
+
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(sqlStatement, connection);
+                cmd.Parameters.AddWithValue("@AddressID", getID);
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+
+        protected void UpdateButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox5_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void FirstButton_Click(object sender, EventArgs e)
+        {
+            TextBox5.Visible = true;
+            TextBox5.Text = "This is entry number 1";
+        }
+
+    }
+}
